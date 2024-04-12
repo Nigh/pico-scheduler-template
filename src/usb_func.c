98ac5ff789c9f67fd2b7afd1717a6754509005ec
xianii <xianyi.xia@outlook.com>
2024-04-12 14:48:22 +0800

    update cdc

--- src/usb_func.c
+++ src/usb_func.c
@@ -4,9 +4,23 @@
 
 #include "string.h"
 
+volatile bool usb_mounted = false;
 //--------------------------------------------------------------------+
 // CDC Funcs
 //--------------------------------------------------------------------+
+
+__attribute__((weak)) void serial_receive(uint8_t const* buffer, uint16_t bufsize) {
+}
+
+static uint16_t log_depth_max = 0;
+void cdc_log_init(void) {
+	log_depth_max = tud_cdc_n_write_available(0);
+}
+void cdc_log_flush(void) {
+	if(tud_cdc_n_write_available(0) < log_depth_max) {
+		tud_cdc_n_write_flush(0);
+	}
+}
 
 void cdc_task(void) {
 	uint8_t itf;
@@ -14,11 +28,16 @@ void cdc_task(void) {
 		if(tud_cdc_n_available(itf)) {
 			uint8_t buf[64];
 			uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf));
+			serial_receive(buf, count);
 		}
 	}
+	cdc_log_flush();
 }
 
 static void serial_write(uint8_t itf, uint8_t buf[], uint32_t count) {
+	if(!usb_mounted) {
+		return;
+	}
 	if(tud_cdc_n_write_available(itf) < count) {
 		tud_cdc_n_write_flush(itf);
 	}
@@ -26,6 +45,9 @@ static void serial_write(uint8_t itf, uint8_t buf[], uint32_t count) {
 }
 
 void cdc_log_print(char* str) {
+	if(!usb_mounted) {
+		return;
+	}
 	uint16_t len = strlen(str);
 	if(tud_cdc_n_write_available(0) < len) {
 		tud_cdc_n_write_flush(0);
@@ -39,10 +61,12 @@ void cdc_log_print(char* str) {
 
 // Invoked when device is mounted
 void tud_mount_cb(void) {
+	usb_mounted = true;
 }
 
 // Invoked when device is unmounted
 void tud_umount_cb(void) {
+	usb_mounted = false;
 }
 
 // Invoked when usb bus is suspended
@@ -54,7 +78,7 @@ void tud_suspend_cb(bool remote_wakeup_en) {
 
 // Invoked when usb bus is resumed
 void tud_resume_cb(void) {
-	//   blink_interval_ms = tud_mounted() ? BLINK_MOUNTED : BLINK_NOT_MOUNTED;
+	usb_mounted = tud_mounted() ? true : false;
 }
 
 //--------------------------------------------------------------------+
@@ -75,6 +99,15 @@ uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t
 	return 0;
 }
 
+__attribute__((weak)) void hid_receive(uint8_t const* buffer, uint16_t bufsize) {
+}
+void hid_send(uint8_t const* buffer, uint16_t bufsize) {
+	if(!usb_mounted) {
+		return;
+	}
+	tud_hid_report(0, buffer, bufsize);
+}
+
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
 void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
@@ -83,6 +116,7 @@ void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t rep
 	(void)report_id;
 	(void)report_type;
 
+	hid_receive(buffer, bufsize);
 	// echo back anything we received from host
-	tud_hid_report(0, buffer, bufsize);
+	// tud_hid_report(0, buffer, bufsize);
 }
--- src/usb_func.h
+++ src/usb_func.h
@@ -1,8 +1,12 @@
 #ifndef _USB_FUNC_H_
 #define _USB_FUNC_H_
 #include <stdint.h>
+#include <stdbool.h>
 
+extern volatile bool usb_mounted;
+void cdc_log_init(void);
 void cdc_task(void);
 void cdc_log_print(char* str);
+void hid_send(uint8_t const* buffer, uint16_t bufsize);
 
 #endif
